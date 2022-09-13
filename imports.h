e208e5a485d5a43b7b0a0f0176079dd424b5c025
Baanista <110647786+Baanista@users.noreply.github.com>
2022-09-13 14:03:35 -0700

    Update imports.h

--- imports.h
+++ imports.h
@@ -1,8 +1,5 @@
-#ifndef TEST_H    // To make sure you don't declare the function more than once by including the header multiple times.
-#define TEST_H
+#ifndef imports_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define imports_H
 
-
-
-
-
+class kingdom();
 #endif
